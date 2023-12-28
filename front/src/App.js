b1f897739b75fa519b6cea048b9bf7766993aa0b
Sergei190 <kudrasovcergej@gmail.com>
2023-12-28 20:31:24 +0500

    last commit

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import Form from './Form'
+
+function App() {
+
+  return (
+    <div className='grid_wrapper'>
+      <div className='grid'><Form /></div>
+      <div className='grid'><Form /></div>
+      <div className='grid'><Form /></div>
+      <div className='grid'><Form /></div>
+    </div>
+  )
+}
+
+export default App
